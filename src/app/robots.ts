f5e7a64c01ba4c46205a9c81645f98eba7332e21
Agney Nalapat <84612798+Agney-gt@users.noreply.github.com>
2025-04-25 16:46:10 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
         "/mindmap/*"
       ]
     },
-    sitemap: ["https://yt2mindmap.com/sitemap.xml"]
+    sitemap: ["https://www.y2map.com/sitemap.xml"]
   };
 }
