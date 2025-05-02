4f995af5fce1e6818ad48e1cc4714e2964a9d8e8
Agney <agneynalapat123@gmail.com>
2025-05-02 10:08:31 +0530

    additional webhooks

--- src/app/api/yt-transcript-webhook-old/route.ts
+++ src/app/api/yt-transcript-webhook-old/route.ts
@@ -24,11 +24,26 @@ export async function POST(request: Request) {
     'https://www.taskade.com/webhooks/flow/01JSG34CAK106CGJD7H01NZ8MP',
     'https://www.taskade.com/webhooks/flow/01JSG3568Z2VQQTPNFCNAMPV96'    
   ];
-  const defaultWebhookUrl = 'https://www.taskade.com/webhooks/flow/01JQB6NWP99Q61ZTVQMFH52XB6';
+  const TextWebhookUrls = ['https://www.taskade.com/webhooks/flow/01JQB6NWP99Q61ZTVQMFH52XB6',
+    'https://www.taskade.com/webhooks/flow/01JT7M368F7GPA2DDNFFXF3PRD',
+    'https://www.taskade.com/webhooks/flow/01JT7M34WRWJX3C3C7CSQ9W282',
+    'https://www.taskade.com/webhooks/flow/01JT7M33GDZWEEX5KW5C198RRH',
+    'https://www.taskade.com/webhooks/flow/01JT7M32F1807Z8PK5GZ4ZPZ0K',
+    'https://www.taskade.com/webhooks/flow/01JT7M30YXSFFE4C2QVTS5D99V',
+    'https://www.taskade.com/webhooks/flow/01JT7M2ZWDCY5MDBTBN8G3K6C4',
+    'https://www.taskade.com/webhooks/flow/01JT7M2YH99XZ4AK9QDBM5ZQ2A',
+    'https://www.taskade.com/webhooks/flow/01JT7M2XDJB0E0ZV82DP1S748M',
+    'https://www.taskade.com/webhooks/flow/01JT7M2W8M195HQX95NYB36HGY',
+    'https://www.taskade.com/webhooks/flow/01JT7M2V19G6V67PJ9C75NZ833',
+    'https://www.taskade.com/webhooks/flow/01JT7M2SHVQBPY5SYHQQ8V6WSP',
+    'https://www.taskade.com/webhooks/flow/01JT7M2R8RG7E3G2AAMJMR1BC9',
+    'https://www.taskade.com/webhooks/flow/01JT7MGTHRSSC73EJRDS4MRVJB',
+    'https://www.taskade.com/webhooks/flow/01JT7MGRJEKZK73SHY6NXRC51N'
+  ]
   const index = dbLength % youtubeWebhookUrls.length; 
   const webhookUrl = isYoutubeLink
   ? youtubeWebhookUrls[index]
-  : defaultWebhookUrl;
+  : TextWebhookUrls[index];
 
   try {
     const response = await fetch(webhookUrl, {
